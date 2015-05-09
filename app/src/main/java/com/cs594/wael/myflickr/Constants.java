6952d605cbde41a077b40e0db30bd5b2851b605c
wael-alhamwi <eng.waelalhamwi@gmail.com>
2015-05-08 21:56:08 -0700

    Lab 3: MyFlickr, Part 1

---
+++ app/src/main/java/com/cs594/wael/myflickr/Constants.java
@@ -0,0 +1,17 @@
+package com.cs594.wael.myflickr;
+
+/**
+ * Created by Wael on 5/8/2015.
+ */
+public class Constants {
+    public static final int NUM_PHOTOS = 10;
+    public static final String TAG = "my code";
+    public static final String API_KEY = "83f48f587c5435e86079eb69a56b2be7";
+    public static final String SECRET = "95c78cd99275fa79";
+    //    public static final String LATITUDE = "34.0672280";
+//    public static final String LONGITUDE = "-118.1667410";
+    public static final String RADIUS = "1";
+
+    public static final String LATITUDE = "40.7127837";
+    public static final String LONGITUDE = "-74.00594130000002";
+}
